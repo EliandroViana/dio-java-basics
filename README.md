6b604e07c94ff8e3892d0afce5bc6b5059137b77
synoap <devsynoap@gmail.com>
2025-02-12 10:41:06 -0300

    feat: modified the types and variables

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dio-java-basics
-Repo with examples of Java Basics
+
+Repo with examples of Java Basics with a lot of comments to explain the basics concepts of Java.
+I'm just saving this repo to can I have a doc to learn the Java Language.
--- types-and-variables/src/TypesVariables.java
+++ types-and-variables/src/TypesVariables.java
@@ -1,5 +1,28 @@
+// Some comments are to explain some concepts.
+// It's help me recognize about important concepts.
+import java.util.Locale;
+
 public class TypesVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Locale.setDefault(Locale.US);
+
+        // Primitive types
+        int age = 32;
+        double income = 4578.32;
+        String zipcode = "0457654-40"; // if the field aren't a math operator don't use integers
+        String name = "John Doe";
+        String city = "New York";
+        final double VALUE_PI = 3.14; // const variable, need be declared with reserved word final and with UpperCase.
+        String cpf = "057.515.545-45"; // if the field aren't a math operator don't use integers
+        long cpf2 = 4657651654654L;
+
+        // casting concept
+        short numbShort = 1;
+        int normalNumb = numbShort;
+        short numbShort2 = (short) normalNumb; // if you're trying this, it's only possible if we're converting the type to short, declaring.
+
+        System.out.printf("My name is %s, I'm %d years old, I make %.2f and I live in %s%n", name, age, income,
+                zipcode, city);
     }
 }
