3496a83db45fb423328e2f667c209a2b338645e7
synoap <devsynoap@gmail.com>
2025-02-17 13:51:32 -0300

    feat: add javadoc

---
+++ learning-java-basics/src/edu/eliandro/javadoc/Comments.java
@@ -0,0 +1,16 @@
+package edu.eliandro.javadoc;
+
+public class Comments {
+
+	// This is a single line comment.
+
+	/**
+	 * This is a multi-line comment,
+	 * or a comment block of documentation.
+	 */
+
+	 /**
+	  * Command to generate a docmentation of the project. 
+	  * cmd: javadoc -encoding UTF-8 -docendoing ISO-8859-1 -d ../docs src/*.java
+	  */
+}
---
+++ learning-java-basics/src/edu/eliandro/javadoc/Document.java
@@ -0,0 +1,28 @@
+package edu.eliandro.javadoc;
+
+public class Document {
+
+	/**
+	 * <h1> Calculadora</h1>
+	 * Aqui está a descrição da calculadora.
+	 *
+	 * @author Eliandro
+	 * @version 1.0
+	 * @since 2025-17-02
+	 */
+
+	 public class Calculator {
+		/**
+		 * <h2> Soma</h2>
+		 * Aqui está a descrição da soma.
+		 *
+		 * @param num1 O primeiro número
+		 * @param num2 O segundo número
+		 * @return A soma dos dois números
+		 */
+		 public int add(int num1, int num2) {
+			 return num1 + num2;
+		 }
+	 }
+
+}
