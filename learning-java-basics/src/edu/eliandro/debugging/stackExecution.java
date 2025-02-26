6a67102147822c9d9b033eac0e9c02dbd5fb4f80
synoap <devsynoap@gmail.com>
2025-02-26 13:58:37 -0300

    feat: add how is stack Execution in Java Program

---
+++ learning-java-basics/src/edu/eliandro/debugging/stackExecution.java
@@ -0,0 +1,47 @@
+package edu.eliandro.debugging;
+
+public class stackExecution {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main init the program!");
+        methodA();
+
+        System.out.println();
+        System.out.println("The end, after exec all the methods calls!");
+    }
+
+    static void methodA() {
+
+        System.out.println();
+        System.out.println("This is the method A result!");
+
+        String[] fruits = new String[]{"morango", "uva", "laranja"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+        methodB();
+    }
+
+    static void methodB() {
+
+        System.out.println();
+        System.out.println("This is the method B result!");
+        for (int i = 0; i < 3; i++) System.out.println(i);
+        methodC();
+    }
+
+    static void methodC() {
+
+        System.out.println();
+        System.out.println("This is the method C result!");
+        Thread.dumpStack(); // force the exception; Stack Trace
+
+        int i = 0;
+        while (i < 3){
+            System.out.println("Index: " + i);
+            i++;
+        }
+    }
+}
